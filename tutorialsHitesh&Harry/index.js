3e66284a0c2fdd3cdd78053a28d7b34e6a7194e7
vikash <vikashmalik83@gmail.com>
2025-06-09 06:15:32 +0530

    clouser / and lexical scope finsed the basic

--- tutorialsHitesh&Harry/index.js
+++ tutorialsHitesh&Harry/index.js
@@ -609,7 +609,72 @@ function User(email, password){
    console.log(tea.email);
 
 
-   //
+   //clouser and lexical scoping ///icase the inner function's full lexical scope will transefer to the meta/ parent function 
+   // re do10:02
+
+
+
+// function init() {
+    //     let name = "Mozilla"; 
+    //     function displayName() {
+    //         console.log(name); 
+    //     }
+    //     displayName();
+    // }
+    // init();
+
+    // function outer(){
+    //     let username = "hitesh"
+    //     console.log("OUTER", secret);
+    //     function inner(){
+    //         let secret = "my123"
+    //         console.log("inner", username);
+    //     }
+    //     function innerTwo(){
+    //         console.log("innerTwo", username);
+    //         console.log(secret);
+    //     }
+    //     inner()
+    //     innerTwo()
+
+    // }
+    // outer()
+    // console.log("TOO OUTER", username);
+
+
+    // function makeFunc() {
+    //     const name = "Mozilla";
+    //     function displayName() {
+    //         console.log(name);
+    //     }
+    //     return displayName;
+    // }
+
+    // const myFunc = makeFunc();
+    // myFunc();
+
+    </script>
+    <script>
+        // document.getElementById("orange").onclick = function(){
+        //     document.body.style.backgroundColor = `orange`
+        // }
+        // document.getElementById("green").onclick = function(){
+        //     document.body.style.backgroundColor = `green`
+        // }
+
+        function clickHandler(color){
+            // document.body.style.backgroundColor = `${color}`
+
+            return function(){
+                document.body.style.backgroundColor = `${color}`
+            }
+        }
+
+        document.getElementById('orange').onclick = clickHandler("orange")
+        document.getElementById('green').onclick = clickHandler("green")
+
+    </script>
+
 
 
 
@@ -629,6 +694,7 @@ function User(email, password){
 
 
 // topics to cover own highger order function in js 
+// function refrance and function call
 
 
 
