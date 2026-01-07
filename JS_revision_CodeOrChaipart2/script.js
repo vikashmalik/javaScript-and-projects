e4de8a1c07dd2a5457bf96550e5c38a66a36edc0
vikash <vikashmalik83@gmail.com>
2026-01-07 05:41:53 +0530

    Revision js code or chai 7 jan

---
+++ JS_revision_CodeOrChaipart2/script.js
@@ -0,0 +1,6 @@
+//a literal is a fixed, constant value written directly in the source code, representing data like numbers, text, or true/false states
+window.document// is an object; there would be many things after document.loc,url...links
+console.log(document.links);//node collection,html collection would give the html collection not array , we would be able to convert them in arr ; so they also have property link map etc
+  
+
+console.log("HEllo hitesh code or chai");
